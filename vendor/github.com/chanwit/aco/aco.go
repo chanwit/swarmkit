4d2690f13e844696c2180d2faa843fd647bb80e9
Chanwit Kaewkasi <chanwit@gmail.com>
2016-11-04 17:12:15 +0700

    vendoring chanwit/aco

---
+++ vendor/github.com/chanwit/aco/aco.go
@@ -0,0 +1,263 @@
+package aco
+
+import (
+	"math"
+	"math/rand"
+	"sort"
+
+	"github.com/docker/swarmkit/api"
+	"github.com/docker/swarmkit/manager/scheduler"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/skelterjohn/go.matrix"
+)
+
+const (
+	MB = 1024 * 1024
+	GB = 1024 * MB
+
+	CORE = 1000 * 1000 * 1000
+
+	MEM_REF = 16 * GB
+	CPU_REF = 4 * CORE
+
+	PLAN_SIZE = 2048
+)
+
+const (
+	W_c = 0.5
+	W_m = 0.5
+)
+
+type Config struct {
+	Ants  int
+	Q     float64
+	Rho   float64
+	Alpha float64
+	Beta  float64
+}
+
+type Plan [PLAN_SIZE]int
+
+func P(n scheduler.NodeInfo) float64 {
+	e := 0.0
+	R_c := float64(n.AvailableResources.NanoCPUs) / float64(n.Description.Resources.NanoCPUs)
+	R_m := float64(n.AvailableResources.MemoryBytes) / float64(n.Description.Resources.MemoryBytes)
+	log.Debugf("R_c: %f, R_m: %f\n", R_c, R_m)
+	return (W_c * (R_c + e)) + (W_m * (R_m + e))
+}
+
+func greedyInit(taskGroup map[string]*api.Task, nodes []scheduler.NodeInfo) *matrix.DenseMatrix {
+	keys := []string{}
+	for k := range taskGroup {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	// row, col
+	TAU := matrix.Zeros(len(taskGroup), len(nodes))
+	// fill zero as resource value
+	for i := 0; i < TAU.Rows(); i++ {
+		for j := 0; j < TAU.Cols(); j++ {
+			if TAU.Get(i, j) == 0.0 {
+				TAU.Set(i, j, P(nodes[j]))
+			}
+		}
+	}
+
+	col := 0
+	for row, k := range keys {
+		task := taskGroup[k]
+		node := nodes[col]
+
+		// TODO: if assign OK
+		// greedy should assign tasks and reduce the resource
+		node.AvailableResources.MemoryBytes -= task.Spec.Resources.Reservations.MemoryBytes
+		node.AvailableResources.NanoCPUs -= task.Spec.Resources.Reservations.NanoCPUs
+		nodes[col] = node
+
+		tau := P(node)
+		TAU.Set(row, col, tau)
+
+		// Simply RR over nodes
+		col = (col + 1) % len(nodes)
+	}
+
+	return TAU
+}
+
+func taskFitsNode(task *api.Task, node scheduler.NodeInfo) bool {
+	t := task.Spec.Resources.Reservations
+	n := node.AvailableResources
+	return (n.MemoryBytes >= t.MemoryBytes) && (n.NanoCPUs >= t.NanoCPUs)
+}
+
+func applyPlan(plan []int, tasks map[string]*api.Task, nodes []scheduler.NodeInfo) []scheduler.NodeInfo {
+	keys := []string{}
+	for k := range tasks {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	plan_idx := 0
+	for _, k := range keys {
+		node_idx := plan[plan_idx]
+		node := nodes[node_idx]
+		task := tasks[k]
+
+		node.AvailableResources.MemoryBytes -= task.Spec.Resources.Reservations.MemoryBytes
+		node.AvailableResources.NanoCPUs -= task.Spec.Resources.Reservations.NanoCPUs
+
+		nodes[node_idx] = node
+		plan_idx++
+	}
+
+	return nodes
+}
+
+func Optimize(taskGroup map[string]*api.Task, nodes []scheduler.NodeInfo, config *Config) (float64, []int) {
+	ANTS := config.Ants
+	Q := config.Q
+	RHO := config.Rho
+	ALPHA := config.Alpha
+	BETA := config.Beta
+
+	refResources := make([]api.Resources, len(nodes))
+	for i, node := range nodes {
+		refResources[i] = api.Resources{
+			MemoryBytes: node.AvailableResources.MemoryBytes,
+			NanoCPUs:    node.AvailableResources.NanoCPUs,
+		}
+	}
+
+	TAU_0 := greedyInit(taskGroup, nodes)
+	TAU := TAU_0.Copy()
+
+	taskNames := []string{}
+	for name := range taskGroup {
+		taskNames = append(taskNames, name)
+	}
+	sort.Strings(taskNames)
+
+	plans := make(map[Plan]int)
+	for i := 0; i < ANTS; i++ {
+		var plan Plan
+		log.Infof("== ANT: %d\n", i)
+
+		// reset
+		for n, refResource := range refResources {
+			node := nodes[n]
+			node.AvailableResources.MemoryBytes = refResource.MemoryBytes
+			node.AvailableResources.NanoCPUs = refResource.NanoCPUs
+			nodes[n] = node
+		}
+
+		// shuffle node orders
+		nodeOrders := make([]int, len(nodes))
+		for i := 0; i < len(nodes); i++ {
+			nodeOrders[i] = i
+		}
+		shuffleInts(nodeOrders)
+		log.Debugf("node order=%v\n", nodeOrders)
+
+		// loop over tasks
+		for task_idx, name := range taskNames {
+
+			ph := make([]float64, len(nodes))
+			ph_sum := 0.0
+
+			task := taskGroup[name]
+
+			// ant tries to put the task on to node
+			for order := 0; order < len(nodeOrders); order++ {
+				// pick order randomly
+				node_idx := nodeOrders[order]
+
+				node := nodes[node_idx]
+				tau := TAU.Get(task_idx, node_idx)
+				log.Debugf("tau=%f\n", tau)
+				log.Debugf("node mem=%d\n", node.AvailableResources.MemoryBytes)
+				log.Debugf("node cpu=%d\n", node.AvailableResources.NanoCPUs)
+
+				if taskFitsNode(task, node) {
+					node.AvailableResources.MemoryBytes -= task.Spec.Resources.Reservations.MemoryBytes
+					node.AvailableResources.NanoCPUs -= task.Spec.Resources.Reservations.NanoCPUs
+				} else {
+					// TODO need to RR over nodes
+					log.Debug("task not fit node")
+					continue
+				}
+
+				nu := P(node)
+				log.Debugf("nu=%f\n", nu)
+				ph[node_idx] = math.Pow(tau, ALPHA) * math.Pow(nu, BETA)
+				ph_sum += ph[node_idx]
+				// log.Debugf("ph[%d]=%f\n", node_idx, ph[node_idx])
+				// log.Debugf("ph_sum[%d]=%f\n", node_idx, ph_sum)
+			}
+
+			p := make([]float64, len(nodes))
+			for node_idx := 0; node_idx < len(nodes); node_idx++ {
+				p[node_idx] = (ph[node_idx] / ph_sum)
+			}
+			log.Debugf("ph = %v\n", ph)
+			log.Debugf("p  = %v\n", p)
+
+			j := -1
+			if rand.Float64() >= Q {
+				j = arg_max(p)
+			} else {
+				j = pick(p)
+			}
+
+			plan[task_idx] = j
+			log.Debugf("j = %d\n", j)
+
+			if j < 0 {
+				// not fit
+				// what to do?
+			} else {
+				TAU.Set(task_idx, j, P(nodes[j]))
+
+				log.Info("vaporize pheromone")
+				if task_idx < len(taskNames)-1 {
+					for j := 0; j < TAU.Cols(); j++ {
+						TAU.Set(task_idx+1, j, (1.0-RHO)*TAU.Get(task_idx, j))
+					}
+					TAU.Set(task_idx+1, j, (1.0-RHO)*P(nodes[j]))
+				}
+
+				log.Info("reduce the chosen resource")
+				node := nodes[j]
+				node.AvailableResources.MemoryBytes -= task.Spec.Resources.Reservations.MemoryBytes
+				node.AvailableResources.NanoCPUs -= task.Spec.Resources.Reservations.NanoCPUs
+				nodes[j] = node
+			}
+
+		} // task loop
+		print(TAU)
+		print(probTran(TAU))
+
+		if _, exist := plans[plan]; exist {
+			plans[plan]++
+		} else {
+			plans[plan] = 1
+		}
+	}
+
+	max := 0
+	var max_plan Plan
+	for plan, count := range plans {
+		// fmt.Printf("%v = %d\n", plan[0:len(taskGroup)], count)
+		if count > max {
+			max = count
+			max_plan = plan
+		}
+	}
+
+	// fmt.Printf("max  = %d\n", max)
+	// fmt.Printf("plan = %v\n", max_plan[0:len(taskGroup)])
+	confidence := float64(max) / float64(ANTS)
+
+	return confidence, max_plan[0:len(taskGroup)]
+}
---
+++ vendor/github.com/chanwit/aco/utils.go
@@ -0,0 +1,71 @@
+package aco
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"text/tabwriter"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/skelterjohn/go.matrix"
+)
+
+func arg_max(items []float64) int {
+	max_val := -1.0
+	max_i := -1
+	for i, item := range items {
+		if max_val < item {
+			max_val = item
+			max_i = i
+		}
+	}
+	return max_i
+}
+
+func pick(items []float64) int {
+	p := rand.Float64()
+	cumulativeProbability := 0.0
+	for i, item := range items {
+		cumulativeProbability += item
+		if p <= cumulativeProbability {
+			return i
+		}
+	}
+	return 0
+}
+
+func shuffleInts(slc []int) {
+	N := len(slc)
+	for i := 0; i < N; i++ {
+		// choose index uniformly in [i, N-1]
+		r := i + rand.Intn(N-i)
+		slc[r], slc[i] = slc[i], slc[r]
+	}
+}
+
+func probTran(mat *matrix.DenseMatrix) *matrix.DenseMatrix {
+	ret := mat.Copy()
+	for i := 0; i < mat.Rows(); i++ {
+		sum := 0.0
+		for j := 0; j < mat.Cols(); j++ {
+			sum += mat.Get(i, j)
+		}
+		for j := 0; j < mat.Cols(); j++ {
+			ret.Set(i, j, mat.Get(i, j)/sum)
+		}
+	}
+	return ret
+}
+
+func print(mat *matrix.DenseMatrix) {
+	buf := new(bytes.Buffer)
+	w := tabwriter.NewWriter(buf, 10, 4, 0, ' ', 0)
+	for i := 0; i < mat.Rows(); i++ {
+		for j := 0; j < mat.Cols(); j++ {
+			fmt.Fprintf(w, "%.6f\t", mat.Get(i, j))
+		}
+		fmt.Fprint(w, "\n")
+	}
+	w.Flush()
+	log.Debugln(buf.String())
+}
